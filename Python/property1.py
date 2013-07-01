3e536158ae63ca08fad4c0a1af0052bf36b672a7
ajlopez <webmaster@ajlopez.com>
2013-07-01 16:23:22 -0300

    super, property python samples

---
+++ Python/property1.py
@@ -0,0 +1,10 @@
+class Rectangle:
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+    def setSize(self, size):
+        self.width, self.height = size
+    def getSize(self):
+        return self.width, self.height
+        
+    size = property(getSize, setSize)
